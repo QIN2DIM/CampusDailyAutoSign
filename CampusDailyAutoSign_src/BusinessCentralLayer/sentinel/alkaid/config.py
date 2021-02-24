36a4810c30fa76c9a28e57fcfde58c32cc8c9853
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2021-02-24 19:22:51 +0800

    .

--- CampusDailyAutoSign_src/BusinessCentralLayer/sentinel/alkaid/config.py
+++
@@ -1,4 +0,0 @@
-from nonebot.config import Config
-
-Config.superusers = {'', }
-Config.command_start = {"!", "£¡", "/"}
--- CampusDailyAutoSign_src/main.py
+++
@@ -1,5 +0,0 @@
-from BusinessCentralLayer.middleware.control import Interface as app
-
-# 今日校园疫情体温检测－－自动上报脚本
-if __name__ == '__main__':
-    app.run()
